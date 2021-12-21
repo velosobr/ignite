e74283aa42182a4334a8b086eb100ab1c3cb250b
lino <linoc.veloso@gmail.com>
2021-12-21 20:37:18 -0300

    Adicionando os tipos [parte 1]

--- node/typescript/src/server.ts
+++ node/typescript/src/server.ts
@@ -1,11 +1,8 @@
 import express from 'express';
+import { createCourse } from './routes';
 
 const app = express();
 
 
-app.get("/", (req, res) => {
-   return res.json({
-      message: "Hello World!"
-   })
-})
+app.get("/", createCourse)
 app.listen(3333);
