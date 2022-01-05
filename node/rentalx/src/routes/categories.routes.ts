be4b0f40cd931677d6baf27b8c3298dd66f61db6
lino <linoc.veloso@gmail.com>
2022-01-05 11:35:18 -0300

    Inserindo tipagem para categoria

--- node/rentalx/src/routes/categories.routes.ts
+++ node/rentalx/src/routes/categories.routes.ts
@@ -1,13 +1,22 @@
 import { Router } from 'express';
-import { v4 as uuidV4 } from 'uuid'
+
+import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
 
 
-const categories = [];
+const categories: Category[] = [];
+
 categoriesRoutes.post("/", (req, res) => {
    const { name, description } = req.body;
-const category = { name, description, id: uuidV4() }
+   const category = new Category();
+
+   Object.assign(category, {
+      name,
+      description,
+      created_at: new Date()
+   })
+
    categories.push(category);
 
    return res.status(201).json(category);
