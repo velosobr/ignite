cb0f03cba5ee69ddcff8f8abfd371dee6c57e4c1
lino <linoc.veloso@gmail.com>
2022-01-05 09:33:46 -0300

    Inserindo ID com uuid

--- node/rentalx/src/routes/categories.routes.ts
+++ node/rentalx/src/routes/categories.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
+import { v4 as uuidV4 } from 'uuid'
 
 const categoriesRoutes = Router();
 
 
 const categories = [];
-categoriesRoutes.post("/categories", (req, res) => {
+categoriesRoutes.post("/", (req, res) => {
    const { name, description } = req.body;
+const category = { name, description, id: uuidV4() }
+   categories.push(category);
 
-   categories.push({ name, description });
-
-   return res.status(201).send();
+   return res.status(201).json(category);
 })
 
 export { categoriesRoutes }
--- node/rentalx/src/server.ts
+++ node/rentalx/src/server.ts
@@ -4,20 +4,7 @@ import { categoriesRoutes } from "./routes/categories.routes";
 const app = express();
 
 app.use(express.json())
-app.use(categoriesRoutes)
-app.get("/", (req, res) => {
-   return res.json({
-      message: "Hello World"
-   })
-})
 
-app.post("/courses", (req, res) => {
-   const { name } = req.body
-
-   return res.json({
-      name
-   })
-
-})
+app.use("/categories", categoriesRoutes)
 
 app.listen(3333, () => console.log("server is Running!"));
