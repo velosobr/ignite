808ed55d36183a1fd2eac3b11da8a16fda4c595f
lino <linoc.veloso@gmail.com>
2022-01-06 14:51:23 -0300

    Criando repositório de categoria

--- node/rentalx/src/routes/categories.routes.ts
+++ node/rentalx/src/routes/categories.routes.ts
@@ -1,25 +1,21 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { Category } from '../model/Category';
+import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
+const categoriesRepository = new CategoriesRepository();
 
 const categories: Category[] = [];
 
 categoriesRoutes.post("/", (req, res) => {
-   const { name, description } = req.body;
-   const category = new Category();
+  const { name, description } = req.body;
+  categoriesRepository.create({
+    name,
+    description,
+  });
 
-   Object.assign(category, {
-      name,
-      description,
-      created_at: new Date()
-   })
+  return res.status(201).send();
+});
 
-   categories.push(category);
-
-   return res.status(201).json(category);
-})
-
-export { categoriesRoutes }
+export { categoriesRoutes };
--- node/rentalx/src/server.ts
+++ node/rentalx/src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
+
 import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/categories", categoriesRoutes)
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("server is Running!"));
