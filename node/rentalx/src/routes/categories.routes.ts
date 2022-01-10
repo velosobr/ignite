f4636ef35817283ae3803d61060cf632e79d87eb
lino <linoc.veloso@gmail.com>
2022-01-09 23:23:21 -0300

    Using single responsability principle

--- node/rentalx/src/routes/categories.routes.ts
+++ node/rentalx/src/routes/categories.routes.ts
@@ -2,6 +2,7 @@ import { response, Router } from "express";
 
 import { Category } from "../model/Category";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -11,6 +12,9 @@ const categories: Category[] = [];
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+  createCategoryService.execute({ name, description });
   return res.status(201).send();
 });
 
--- node/rentalx/src/services/CreateCategoryService.ts
+++ node/rentalx/src/services/CreateCategoryService.ts
@@ -1,18 +1,25 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
 interface IRequest {
   name: string;
   description: string;
 }
 
+/**
+ * [x] - Definir o tipo de retorno
+ * [x] - Alterar o retorno de erro
+ * [x] - Acessar o repositório
+ */
 class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
   execute({ description, name }: IRequest) {
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
 
     if (categoryAlreadyExists) {
-      return res.status(400).json({
-        error: "the category already exists",
-      });
+      throw new Error("the category already exists!");
     }
-    categoriesRepository.create({
+    this.categoriesRepository.create({
       name,
       description,
     });
