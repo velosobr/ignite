366c171d2dee7210fb7a121a42e8bc931b81e52f
lino <linoc.veloso@gmail.com>
2022-01-10 23:50:36 -0300

    WIP - Using LSP

---
+++ node/rentalx/src/repositories/PostgresCategoriesRepository.ts
@@ -0,0 +1,15 @@
+import { Category } from "../model/Category";
+import { ICategoriesRepository } from "./ICategoriesRepository";
+
+class PostgresCategoriesRepository implements ICategoriesRepository {
+  findByName(name: string): Category {
+    throw new Error("Method not implemented.");
+  }
+  list(): Category[] {
+    throw new Error("Method not implemented.");
+  }
+  create(name: string, description: string): void {
+    throw new Error("Method not implemented.");
+  }
+}
+export { PostgresCategoriesRepository };
