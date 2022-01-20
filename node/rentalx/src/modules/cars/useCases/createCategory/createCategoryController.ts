f5116a6c3f9e58dfc59a85480aea4a85720afb6b
Lino Veloso <linoc.veloso@gmail.com>
2022-01-20 20:41:46 -0300

    create UseCase for category

--- node/rentalx/src/modules/cars/useCases/createCategory/createCategoryController.ts
+++ node/rentalx/src/modules/cars/useCases/createCategory/createCategoryController.ts
@@ -4,10 +4,12 @@ import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 class CreateCategoryController {
   constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
+
   handle(req: Request, res: Response): Response {
     const { name, description } = req.body;
 
     this.createCategoryUseCase.execute({ name, description });
+
     return res.status(201).send();
   }
 }
--- node/rentalx/src/modules/cars/useCases/createCategory/index.ts
+++ node/rentalx/src/modules/cars/useCases/createCategory/index.ts
@@ -3,7 +3,9 @@ import { CreateCategoryController } from "./createCategoryController";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 const categoriesRepository = new CategoriesRepository();
+
 const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+
 const createCategoryController = new CreateCategoryController(
   createCategoryUseCase
 );
