8fcc5c9ed6a7cfc22711ff88bdec977d351d876b
Lino Veloso <linoc.veloso@gmail.com>
2022-01-20 21:23:45 -0300

    refactory list category/create list categoryUseCase [parte2]

--- node/rentalx/src/routes/categories.routes.ts
+++ node/rentalx/src/routes/categories.routes.ts
@@ -1,18 +1,16 @@
-import { response, Router } from "express";
+import { Router } from "express";
 
-import { Category } from "../modules/cars/model/Category";
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoryController } from "../modules/cars/useCases/ListCategory";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get("/", (req, res) => {
-
+  return listCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
