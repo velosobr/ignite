0e425f51fade9ac489ae396c1111421d001f6e1f
Lino Veloso <linoc.veloso@gmail.com>
2022-01-22 20:56:02 -0300

    singleton impl finished with bugs

--- node/rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ node/rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,5 +1,4 @@
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
-import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
   name: string;
