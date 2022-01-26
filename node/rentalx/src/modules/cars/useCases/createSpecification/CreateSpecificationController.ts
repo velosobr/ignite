29e6085fd3a02c9f55740072538dc4b7b108519d
Lino Veloso <linoc.veloso@gmail.com>
2022-01-26 00:17:12 -0300

    creating Specific UseCase and Controller

---
+++ node/rentalx/src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+class CreateSecificationController {
+  constructor();
+
+  handle(request: Request, response: Response): Response {}
+}
+
+export { CreateSecificationController };
