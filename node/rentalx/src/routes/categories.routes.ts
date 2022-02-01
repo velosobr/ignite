7688eb9fb6631e072bf497a30a37f0d6da47e9dd
Lino Veloso <linoc.veloso@gmail.com>
2022-02-01 13:16:03 -0300

    feat: upload done

--- node/rentalx/src/routes/categories.routes.ts
+++ node/rentalx/src/routes/categories.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoryController } from "../modules/cars/useCases/ListCategory";
 
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
@@ -13,4 +17,10 @@ categoriesRoutes.get("/", (req, res) => {
   return listCategoryController.handle(req, res);
 });
 
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  const { file } = req;
+  console.log(file);
+
+  return res.send();
+});
 export { categoriesRoutes };
