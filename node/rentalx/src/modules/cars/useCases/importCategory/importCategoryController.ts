dae669d1796bac08d9d327b1f85d5745bc105be4
Lino Veloso <linoc.veloso@gmail.com>
2022-02-04 20:45:37 -0300

    create import category controller

--- node/rentalx/src/modules/cars/useCases/importCategory/importCategoryController.ts
+++ node/rentalx/src/modules/cars/useCases/importCategory/importCategoryController.ts
@@ -1,3 +1,10 @@
-class ImportCategoryController {}
+class ImportCategoryController {
+  handle(request: Request, response: Response): Response {
+    const { file } = request;
+    console.log(file);
+
+    return response.send();
+  }
+}
 
 export { ImportCategoryController };
