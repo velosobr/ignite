97d1801eec58accb21f7db706d0e3e0a0a9f2339
Lino Veloso <linoc.veloso@gmail.com>
2022-02-16 11:59:08 -0300

    end of swagger setup

--- node/rentalx/src/server.ts
+++ node/rentalx/src/server.ts
@@ -2,12 +2,13 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
