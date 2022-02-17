d32ce7281dcd94690604532429cb12d68e952331
Lino Veloso <linoc.veloso@gmail.com>
2022-02-17 12:22:11 -0300

    list all users and handlers in exception cases

--- node/desafios/ignite-template-introducao-ao-SOLID/src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ node/desafios/ignite-template-introducao-ao-SOLID/src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,11 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-this.listAllUsersUseCase.execute()  }
+    const { user_id } = request.body;
+
+    this.listAllUsersUseCase.execute(user_id);
+    return response.status(200).send();
+  }
 }
 
 export { ListAllUsersController };
