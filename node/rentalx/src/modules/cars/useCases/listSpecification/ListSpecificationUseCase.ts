86c386c165cf4faa472a66c7c522eb34ea176e5b
Lino Veloso <linoc.veloso@gmail.com>
2022-04-20 21:45:40 -0300

    implement final spec implementation

---
+++ node/rentalx/src/modules/cars/useCases/listSpecification/ListSpecificationUseCase.ts
@@ -0,0 +1,19 @@
+import { inject, injectable } from "tsyringe";
+
+import { Specification } from "../../entities/Specification";
+import { ISpecificationRepository } from "../../repositories/ISpecificationsRepository";
+
+@injectable()
+class ListSpecificationUseCase {
+  constructor(
+    @inject("SpecificationRepository")
+    private specificationRepository: ISpecificationRepository
+  ) {}
+
+  async execute(): Promise<Specification[]> {
+    const specifications = await this.specificationRepository.list();
+
+    return specifications;
+  }
+}
+export { ListSpecificationUseCase };
--- node/rentalx/src/routes/specifications.routes.ts
+++ node/rentalx/src/routes/specifications.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { ListSpecificationsController } from "../modules/cars/useCases/listSpecification/ListSpecificationsController";
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
+const listSpecificationController = new ListSpecificationsController();
 
 specificationsRoutes.post("/", createSpecificationController.handle);
+specificationsRoutes.get("", listSpecificationController.handle);
 
 export { specificationsRoutes };
